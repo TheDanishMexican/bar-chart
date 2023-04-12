602a7f7eecd5ca2763a8a9889c2bdd256978144e
TheDanishMexican <danieljchristjansen@gmail.com>
2023-04-12 09:12:28 +0200

    loops

--- app.js
+++ app.js
@@ -3,22 +3,28 @@
 window.addEventListener("load", start);
 
 function start() {
-  displayNewColumnPerSecond();
+  const barChartArray = createArray();
+  displayNewColumnPerSecond(barChartArray);
 }
 
-const barChartArray = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 25, 29, 30, 31, 32, 12, 16, 22, 27, 30, 1, 3, 5,
-];
+function createArray() {
+  let barChartArray = [];
 
-function addAndDelete() {
+  for (let i = 0; i < 40; i++) {
+    barChartArray[i] = getNumberOfCustomers();
+  }
+
+  return barChartArray;
+}
+
+function addAndDelete(barChartArray) {
   barChartArray.push(getNumberOfCustomers());
   barChartArray.shift();
   return barChartArray;
 }
 
-function setBarHeight() {
-  addAndDelete();
+function setBarHeight(barChartArray) {
+  addAndDelete(barChartArray);
   const allColumns = document.querySelectorAll(".column");
   for (let i = 0; i <= allColumns.length; i++) {
     allColumns[i].style.height = `${barChartArray[i]}vw`;
@@ -29,6 +35,6 @@ function getNumberOfCustomers() {
   return Math.floor(Math.random() * 32);
 }
 
-function displayNewColumnPerSecond() {
-  setInterval(setBarHeight, 1000);
+function displayNewColumnPerSecond(barChartArray) {
+  setInterval(() => setBarHeight(barChartArray), 1000);
 }
